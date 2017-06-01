fc8aed91489fd83a50abcce544655d3afe60d4e6
1 <a1@1deMacBook-Pro.local>
2017-06-01 14:23:35 +0800

    Initial commit

---
+++ app/src/main/java/cn/edu/neu/bus_reserving/Login2Activity.java
@@ -0,0 +1,13 @@
+package cn.edu.neu.bus_reserving;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Login2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login2);
+    }
+}
